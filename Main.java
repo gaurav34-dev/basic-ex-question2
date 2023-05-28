a83e3b7c06fb137633e0f6dfc5d02b1ac39efc77
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-05-27 22:49:39 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("please enter two numbers");
+    Scanner s= new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    System.out.println(a);
+    System.out.println(b);
+}
+}
+
+
+//Write a program to print the value given by the user.
